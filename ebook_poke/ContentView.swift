93ea5b8739d5b4839f66595c0cdc0b0de47f1198
andy <andy870622@gmail.com>
2019-10-17 14:48:04 +0800

    Initial Commit

---
+++ ebook_poke/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ebook_poke
+//
+//  Created by User15 on 2019/10/17.
+//  Copyright © 2019 00457158. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
